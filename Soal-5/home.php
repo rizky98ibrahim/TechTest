283b6dfbc356823528ff57a6653311306b0fa158
rizky98ibrahim <rizky98ibrahim@gmail.com>
2023-07-07 16:00:01 +0700

    [update] soal nomor 5

---
+++ Soal-5/home.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+// ! Check if user already logged in
+if (!isset($_SESSION['username'])) {
+    // ! Redirect to login.php
+    header("Location: login.php");
+    exit;
+}
+
+// ! Get username from session
+$username = $_SESSION['username'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi Instansi - Beranda</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Aplikasi Instansi</a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-4">
+        <h1>Welcome <?php echo $username; ?></h1>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
