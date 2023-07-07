039444fdc8650b4e5bd6daa6e6bf828021d8d4c3
rizky98ibrahim <rizky98ibrahim@gmail.com>
2023-07-07 15:44:56 +0700

    [update] soal nomor 4

---
+++ Soal-4/4_looping_condition.php
@@ -0,0 +1,30 @@
+<?php
+// ! Looping Condition
+
+// * Initialize $currentLine
+$currentLine = ""; //
+
+for ($i = 1; $i <= 50; $i++) {
+    // * Check if $i is divisible by 3 and 5
+    if ($i % 3 == 0 && $i % 5 == 0) {
+        $currentLine .= $i . " ";
+        echo "FooBar = " . $currentLine . "\n";
+        $currentLine = "";
+    }
+    // * Check if $i is divisible by 3
+    elseif ($i % 3 == 0) {
+        $currentLine .= $i . " ";
+        echo "Foo = " . $currentLine . "\n";
+        $currentLine = "";
+    }
+    // * Check if $i is divisible by 5
+    elseif ($i % 5 == 0) {
+        $currentLine .= $i . " ";
+        echo "Bar = " . $currentLine . "\n";
+        $currentLine = "";
+    }
+    // * If not divisible by 3 or 5
+    else {
+        $currentLine .= $i . " ";
+    }
+}
